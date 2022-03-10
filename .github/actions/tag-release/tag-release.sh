8ecd6701b6f13d720f01fc2d53e8f56682938451
Yan Zverev <yan.zverev@twosixtech>
2022-03-10 16:13:20 -0500

    Adding tag a release action

---
+++ .github/actions/tag-release/tag-release.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/env bash
+
+if [[ -z $GITHUB_RUN_NUMBER ]]; then
+  echo "Missing GITHUB_RUN_NUMBER"
+  exit 1
+fi
+
+git config user.name "${GITHUB_ACTOR}"
+git config user.email "${GITHUB_ACTOR}@users.noreply.github.com"
+
+if [[ -n $RELEASE_VERSION ]]; then
+  releaseVersion=$RELEASE_VERSION
+  echo "$RELEASE_VERSION" > app.version
+else
+  buildNumber=$GITHUB_RUN_NUMBER
+  majorVersion=`cat version.sbt | cut -d\" -f2 | sed 's/.0-SNAPSHOT//'`
+  releaseVersion="${majorVersion}.${buildNumber}"
+  echo "version in ThisBuild := \"$releaseVersion\"" > version.sbt
+fi
+
+echo "Tagging Release Version: $releaseVersion"
+
+git checkout -b "release-$releaseVersion"
+
+#git add .
+#git commit -m "[CI/CD] Release v$releaseVersion"
+#
+#git tag "v$releaseVersion"
+#git push origin "v$releaseVersion"
